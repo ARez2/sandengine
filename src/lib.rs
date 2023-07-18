74f9ec4751cf5543fddfaa234784060eaf1a53f6
ARez <dark.gaming@fantasymail.de>
2023-07-18 21:48:51 +0200

    Start YAML parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,20 @@
 use std::str::FromStr;
-
+use serde_yaml;
 
 pub fn run() {
+    parse_yaml();
+    return;
     build_compute_shaders();
     sandengine_core::run();
 }
+
+
+fn parse_yaml() {
+    let f = std::fs::read_to_string("materials.yaml").unwrap();
+    let d: serde_yaml::Value = serde_yaml::from_str(&f).unwrap();
+    println!("Read YAML string: {:?}", d.get("materials").unwrap());
+}
+
 
 
 fn build_compute_shaders() {
